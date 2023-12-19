5eaf5c0cf9dba91ca577c1b2ec1e6f1e7525c900
navyney <boontharika_kor@cmu.ac.th>
2023-12-19 16:16:32 +0700

    finished

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+double sum = 0;
+int count = 0;
+double mean,StandardDeviation,n2;
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    while (getline(source,textline))
+    {
+        sum += stof(textline.c_str());
+        n2 += pow(atof(textline.c_str()),2);
+        count++;
+    }
+    mean = sum/count;
+    StandardDeviation = sqrt((n2/count)-pow(mean,2));
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << StandardDeviation;
+}
